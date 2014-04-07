136020d89686bfe94eabdb953a108067bef0f9c4
Philippe Gaultier <pgaultier@sweelix.net>
2014-04-07 16:23:48 +0200

    Adding controller samples

---
+++ protected/controllers/ListTemplateController.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * SiteController.php
+ *
+ * PHP version 5.4+
+ *
+ * @author    Philippe Gaultier <pgaultier@sweelix.net>
+ * @copyright 2010-2014 Sweelix
+ * @license   http://www.sweelix.net/license license
+ * @version   1.0.0
+ * @link      http://www.sweelix.net
+ * @category  controllers
+ * @package   application.controllers
+ */
+
+namespace application\controllers;
+use sweelix\yii1\ext\web\Controller;
+
+/**
+ * This is the basic app controller
+ *
+ * @author    Philippe Gaultier <pgaultier@sweelix.net>
+ * @copyright 2010-2014 Sweelix
+ * @license   http://www.sweelix.net/license license
+ * @version   1.0.0
+ * @link      http://www.sweelix.net
+ * @category  controllers
+ * @package   application.controllers
+ * @since     1.0.0
+ */
+class ListTemplateController extends Controller {
+
+	/**
+	 * Default action
+	 *
+	 * @return void
+	 * @since  1.0.0
+	 */
+	public function actionIndex() {
+		$this->render('index');
+	}
+}
---
+++ protected/views/listTemplate/index.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * index.php
+ *
+ * PHP version 5.4+
+ *
+ * @author    Philippe Gaultier <pgaultier@sweelix.net>
+ * @copyright 2010-2014 Sweelix
+ * @license   http://www.sweelix.net/license license
+ * @version   1.0.0
+ * @link      http://www.sweelix.net
+ * @category  views
+ * @package   application.views.listTemplate
+ */
+
+$this->pageTitle = "Sweelix boiler plate - listTemplate";
+use sweelix\yii1\web\helpers\Html;
+?>
+
+<div class="jumbotron">
+	<h1>Sweelix Boilerplate - listTemplate</h1>
+	<p>
+		This controller / action has been called dynamically.</p>
+	<p>
+		<?php echo Html::link('Jump to index &raquo;', ['site/'], [
+				'class' => 'btn btn-primary btn-lg',
+				'role' => 'button',
+			]);
+		?>
+		<?php echo Html::link('url manuelle', [$this->node->getRoute(), 'tagId' => 50]); ?>
+	</p>
+
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<h2>Current context : </h2>
+			<ul class="list-group">
+				<li class="list-group-item active">
+					<h4 class="list-group-item-heading">Content ID</h4>
+					<p class="list-group-item-text">contentId = <?php echo ($this->contentId===null)?'null':$this->contentId; ?></p>
+				</li>
+				<li class="list-group-item active">
+					<h4 class="list-group-item-heading">Node ID</h4>
+					<p class="list-group-item-text">nodeId = <?php echo ($this->nodeId===null)?'null':$this->nodeId; ?></p>
+				</li>
+				<li class="list-group-item active">
+					<h4 class="list-group-item-heading">Group ID</h4>
+					<p class="list-group-item-text">groupId = <?php echo ($this->groupId===null)?'null':$this->groupId; ?></p>
+				</li>
+				<li class="list-group-item active">
+					<h4 class="list-group-item-heading">Tag ID</h4>
+					<p class="list-group-item-text">tagId = <?php echo ($this->tagId===null)?'null':$this->tagId; ?></p>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
